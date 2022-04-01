6b1b5746b99afe213fce830ae0e77cfa7ff46d52
John Winsor <johnwinsor@gmail.com>
2022-03-31 19:09:06 -0700

    Upgrade tailwind

--- app/gem-farm/tailwind.config.js
+++ app/gem-farm/tailwind.config.js
@@ -1,21 +1,23 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        "imso-violet": "rgba(188,75,188,0.5)",
-        "imso-violet-hover": "rgba(188,75,188,0.8)",
-        "imso-yellow": "rgba(234,232,74)",
-        "imso-greena": "rgba(91,234,189,0.5)",
-        "imso-greenb": "rgba(91,234,189,0.8)",
-        "imso-blue": "rgba(0,208,255,0.7)",
-        "imso-bg-blue": "rgba(0,208,255,0.4)",
-        "imso-bg-gray": "rgba(80,180,180,0.2)",
-      },
-      fontSize: {
-        xxs: ".5rem",
-      },
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                "imso-violet": "rgba(188,75,188,0.5)",
+                "imso-violet-hover": "rgba(188,75,188,0.8)",
+                "imso-yellow": "rgba(234,232,74)",
+                "imso-greena": "rgba(91,234,189,0.5)",
+                "imso-greenb": "rgba(91,234,189,0.8)",
+                "imso-blue": "rgba(0,208,255,0.7)",
+                "imso-bg-blue": "rgba(0,208,255,0.4)",
+                "imso-bg-gray": "rgba(80,180,180,0.2)",
+            },
+            fontSize: {
+                xxs: ".5rem",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [
+        require('@tailwindcss/forms'),
+    ],
 };
